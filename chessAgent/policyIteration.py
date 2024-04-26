989d79722ec7edd1d5597c94786f0eb91ca4e7d2
PeterMinsch <peteminsch.pm@gmail.com>
2024-04-26 14:49:52 -0700

    Made comments and added 3 fuctions: valueIteration(), calculateProbabilityMatrix(), and findAgentPieces

--- chessAgent/policyIteration.py
+++ chessAgent/policyIteration.py
@@ -7,6 +7,31 @@ use value iteration for each red piece
 
 class Agent:
 
+    '''
+    params- 
+    V- initial state values
+    P- Proability matrix
+    nA- Number of actions
+    nS- Number of states
+
+    '''
     def valueIteration():
         pass
-    pass
+    
+
+    '''
+    This matrix will hold objects for each state and their actions. 
+    Each slot holds a probability, reward, the next state, and if the next state holds a terminal state
+    '''
+    def calculateProbabilityMatrix():
+        pass
+
+    '''
+    this will loop through the board and find the agent's pieces and store them in a list.
+    this list will represent our number of states
+    '''
+    
+    def findAgentPieces():
+        pass
+
+    
