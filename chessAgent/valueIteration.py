061d2c0d3264172a8551100439c176502c078556
PeterMinsch <peteminsch.pm@gmail.com>
2024-05-02 23:35:03 -0700

    Made changes to valueIteration on main

--- chessAgent/valueIteration.py
+++ chessAgent/valueIteration.py
@@ -37,18 +37,18 @@ class Agent:
     '''
     def valueIteration(V, P, nA, nS):
         gamma = 0.9
-        new_V = [0]*nS #initialize an array for nS size (new updated values)
-        policy = [0]*nS #initialize an array for nS size (best action for each state)
+        new_V, policy = [0]*nS #initialize an array for nS size (new updated values)
+        
         for s in range(nS): #fill policy and new_v arrays
             new_v = [0]*nA #initialize an array for all actions
             for a in range(nA): #fill action to take array
                 #Let's consider only deterministic policy; P[][] is where all the (state,action)->(next_state,reward) probability, rewards are stored
                 for prob, next_s, reward, terminal in P[s][a]: #
-                    new_v[a] += prob * (reward + gamma * V[next_s]) #(1) fill in the blank: calculate the updated state value for action a at state s
-            new_V[s] = max(new_v) #(2) fill in the blank: update the state value for state s to be the maximum state-action value
-            policy[s] = np.argmax(new_V) #(3) fill in the blank: update the policy for state s to be the action that has the maximum state-action value
+                    new_v[a] += prob * (reward + gamma * V[next_s]) 
+            new_V[s] = max(new_v) 
+            policy[s] = np.argmax(new_V) 
         return new_V, policy
-        # pass
+        
     
 
     '''
